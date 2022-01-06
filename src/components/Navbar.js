ea940aaf25d1743397a15edad367a8015f6b5448
Joseph <josephroby99@gmail.com>
2022-01-06 17:47:20 +0530

    second commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+export default function Navbar() {
+
+    return (
+        <nav>
+            <img src="../images/airbnb.png"
+            className="nav--logo" alt="airbnb logo"/>
+        </nav>
+
+    )
+
+}
